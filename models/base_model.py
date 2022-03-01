9e3259a6e1269e10ef0a768b4ee085be53329227
Camiloarceg <camiloarceg@gmail.com>
2022-03-01 13:17:59 -0500

    progess task 5 nedd fix reloard()

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,8 @@
 
 import uuid
 from datetime import datetime
+from models import storage
+
 
 class BaseModel():
     """ The class template for creating new instances of BaseModel
@@ -18,11 +20,10 @@ class BaseModel():
                     v = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
                 setattr(self, k, v)
         else:
-            self.id = uuid.uuid4()
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            self.name = ""
-            self.my_number = 0
+            storage.new(self)
 
 
     def __str__(self):
@@ -35,12 +36,13 @@ class BaseModel():
             with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ Returns a dictionary containing all keys/values of __dict__ of the instance
         """
-        my_dict = ({'my_number': self.my_number, 'name': self.name,
+        my_dict = {'my_number': self.my_number, 'name': self.name,
                    '__class__': self.__class__.__name__ ,
                    'updated_at': self.updated_at.isoformat("T", "microseconds"),
-                   'id': self.id, 'created_at': self.created_at.isoformat("T", "microseconds")})
+                   'id': self.id, 'created_at': self.created_at.isoformat("T", "microseconds")}
         return my_dict
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,12 +1,40 @@
 #!/usr/bin/python3
 """ Module that serializes instances to a JSON file and deserializes JSON file to instances
 """
+import json
 
 class FileStorage:
     """ a class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances
     """
     __file_path = "file.json"
-    __objects = []
-    
+    __objects = {}
+
     def all(self):
-        return FileStorage.__objects
+        """ returns the dictionary __objects
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """ sets in __objects the obj with key <obj class name>.id
+        """
+        k = obj.__class__.__name__ +'.'+ obj.id
+        self.__objects[k]= obj
+
+    def save(self):
+        """  serializes __objects to the JSON file (path: __file_path)
+        """
+        my_dict = {}
+        for k in self.__objects:
+            my_dict[k] = self.__objects[k].to_dict()
+        with open(self.__file_path, "w") as myFile:
+            json.dump(my_dict, myFile)
+
+    def reload(self):
+        """ deserializes the JSON file to __objects (only if the JSON file (__file_path) exists)
+        """
+        try: 
+            with open(self.__file_path) as json_file:
+                content = json_file.read()
+                self.__objects = json.loads(content)
+        except:
+            pass
